9d799036500df64a5f6b381306c804da5041b23d
mayank <mayank.rawat1@tothenew.com>
2023-01-03 14:14:53 +0530

    added react-router-dom and also create login page

---
+++ src/components/login/login.jsx
@@ -0,0 +1,23 @@
+import "./login.css";
+
+import { Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="inner-card">
+          <img className="img" src="" alt="logo" />
+          <p className="text">Admin Dashboard</p>
+        </div>
+        <div className="button_container">
+          <Link to="/dash">
+            <button className="btn">LOGIN</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
